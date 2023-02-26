b7464f0d18efe790fa7cea6ad45d2af48ecd5fbe
ashley speight <ashleyspeight86@gmail.com>
2023-02-26 18:16:30 +0100

    Complete first two exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, number) {
+  let answer = "";
+  if (number < 0) {
+    return "ERROR";
+  }
+  for (let i = 0; i < number; i++) {
+    answer += string;
+  }
+  return answer;
 };
 
 // Do not edit below this line
