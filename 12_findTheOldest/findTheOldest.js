4dbb71bd563b161d1732a4c59de695c217c1913f
ashley speight <ashleyspeight86@gmail.com>
2023-03-02 21:10:30 +0100

    JSON pt 2

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+  let myArr = [];
+  for (ele in arr) {
+    // console.log(arr[ele]);
+    myArr.push(
+      `{${arr[ele].name}: ${arr[ele].yearOfDeath} - ${arr[ele].yearOfBirth}}}`
+    );
+    // myArr.push();
+    console.log(myArr);
+  }
 };
 
 // Do not edit below this line
