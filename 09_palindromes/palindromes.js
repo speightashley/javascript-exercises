c7037596bfc90fceb8f5e85e60424f0acacc9672
ashley speight <ashleyspeight86@gmail.com>
2023-03-02 20:13:28 +0100

    Do palindrome ex

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (x) {
+  const processed = x.toLowerCase().replace(/[^a-z]/g, "");
+  return processed.split("").reverse().join("") == processed;
 };
 
 // Do not edit below this line
